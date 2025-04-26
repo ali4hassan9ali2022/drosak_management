1b4f7a787e9af8e6001c79d76d4803d06f987f68
alikasrawy <kasrawyali47@gmail.com>
2025-04-26 12:43:12 +0300

    add app styles file to my app

--- lib/Core/Utils/app_color.dart
+++ lib/Core/Utils/app_color.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 abstract class AppColor {
   static Color secondColorBlock = Color(0xff18171C);
+  static Color colorWhite = Color(0xffffffff);
+  static Color primaryColor = Color(0xff8012DC);
+  static Color thirdBlock = Color(0xff47463C);
+  static Color fourBlock = Color(0xff514C4C);
+  static Color colorGray = Color(0xff998C8C);
 }
--- lib/Core/Utils/app_styles.dart
+++ lib/Core/Utils/app_styles.dart
@@ -1,15 +1,73 @@
 import 'package:drosak_management/Core/Utils/app_color.dart';
 import 'package:flutter/material.dart';
+
 abstract class AppStyles {
- static TextStyle styleBold20(context) {
-  return TextStyle(
-    fontSize: getResponsiveFontSize(context, fontSize: 20),
-    fontFamily: "AA-GALAXY",
-    color: AppColor.secondColorBlock,
-    fontWeight: FontWeight.w700,
-  );
- }
+  static TextStyle styleBold20(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontFamily: "AA-GALAXY",
+      color: AppColor.secondColorBlock,
+      fontWeight: FontWeight.w700,
+    );
+  }
+  static TextStyle styleMedium15(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 15),
+      fontFamily: "GE Dinar One",
+      color: AppColor.colorWhite,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle styleMedium20(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontFamily: "GE Dinar One",
+      color: AppColor.primaryColor,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle styleMedium8(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 8),
+      fontFamily: "GE Dinar One",
+      color: AppColor.thirdBlock,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle styleMedium16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: "GE Dinar One",
+      color: AppColor.colorWhite,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle styleMedium10(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 10),
+      fontFamily: "GE Dinar One",
+      color: AppColor.fourBlock,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle styleMedium12(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: "GE Dinar One",
+      color: AppColor.colorWhite,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle styleMedium14(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: "GE Dinar One",
+      color: AppColor.primaryColor,
+      fontWeight: FontWeight.w500,
+    );
+  }
 }
+
 // scale factor
 // responsive fontSiaze
 // min , max
