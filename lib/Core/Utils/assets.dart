f539d15b53f0396b6f2a637a7e32266066129f4c
alikasrawy <kasrawyali47@gmail.com>
2025-04-26 12:18:41 +0300

    add assets file to my app

---
+++ lib/Core/Utils/assets.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAndroidAbove12
+  /// assets/images/android_above_12.png
+  static const String imagesAndroidAbove12 = "assets/images/android_above_12.png";
+
+  /// Assets for imagesAudience
+  /// assets/images/audience.svg
+  static const String imagesAudience = "assets/images/audience.svg";
+
+  /// Assets for imagesBackgroundImage
+  /// assets/images/background_image.png
+  static const String imagesBackgroundImage = "assets/images/background_image.png";
+
+  /// Assets for imagesEducationalStages
+  /// assets/images/educational_stages.svg
+  static const String imagesEducationalStages = "assets/images/educational_stages.svg";
+
+  /// Assets for imagesGroup
+  /// assets/images/Group.svg
+  static const String imagesGroup = "assets/images/Group.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesOnBoardingImage1
+  /// assets/images/on_boarding_image_1.svg
+  static const String imagesOnBoardingImage1 = "assets/images/on_boarding_image_1.svg";
+
+  /// Assets for imagesOnBoardingImage2
+  /// assets/images/on_boarding_image_2.svg
+  static const String imagesOnBoardingImage2 = "assets/images/on_boarding_image_2.svg";
+
+  /// Assets for imagesOnBoardingImage3
+  /// assets/images/on_boarding_image_3.svg
+  static const String imagesOnBoardingImage3 = "assets/images/on_boarding_image_3.svg";
+
+  /// Assets for imagesOnBoardingImage4
+  /// assets/images/on_boarding_image_4.svg
+  static const String imagesOnBoardingImage4 = "assets/images/on_boarding_image_4.svg";
+
+  /// Assets for imagesOnBoardingImage5
+  /// assets/images/on_boarding_image_5.svg
+  static const String imagesOnBoardingImage5 = "assets/images/on_boarding_image_5.svg";
+
+  /// Assets for imagesPaymentIcon
+  /// assets/images/payment_icon.svg
+  static const String imagesPaymentIcon = "assets/images/payment_icon.svg";
+
+  /// Assets for imagesPlaceholderSvg
+  /// assets/images/placeholderSvg.svg
+  static const String imagesPlaceholderSvg = "assets/images/placeholderSvg.svg";
+
+  /// Assets for imagesSplashBorderImage
+  /// assets/images/splash_border_image.png
+  static const String imagesSplashBorderImage = "assets/images/splash_border_image.png";
+
+  /// Assets for imagesSplashBorderImageBottom
+  /// assets/images/splash_border_image_bottom.png
+  static const String imagesSplashBorderImageBottom = "assets/images/splash_border_image_bottom.png";
+
+  /// Assets for imagesStudents
+  /// assets/images/students.svg
+  static const String imagesStudents = "assets/images/students.svg";
+
+  /// Assets for imagesTest
+  /// assets/images/test.png
+  static const String imagesTest = "assets/images/test.png";
+}
+
