5ae577390ef415d7f10eeedac17ea03e0d2982a8
alikasrawy <kasrawyali47@gmail.com>
2025-05-10 10:31:08 +0300

    add build suggestions educatinal stages

--- lib/Featured/Layout/Widgets/custom_search_delegate_educational.dart
+++ lib/Featured/Layout/Widgets/custom_search_delegate_educational.dart
@@ -1,8 +1,11 @@
 import 'package:drosak_management/Core/Utils/app_styles.dart';
+import 'package:drosak_management/Core/Utils/size_config.dart';
 import 'package:drosak_management/Cubit/database_cubit/database_cubit.dart';
+import 'package:drosak_management/Cubit/database_cubit/database_state.dart';
 import 'package:drosak_management/Featured/Layout/Widgets/custom_build_result_educational_stages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomSearchDelegateEducational extends SearchDelegate<String> {
   @override
@@ -33,8 +36,10 @@ class CustomSearchDelegateEducational extends SearchDelegate<String> {
         ? CustomBuildResultEducationalStages()
         : Center(
           child: Text(
-            "ابدأ بالبحث عن البيانات",
-            style: AppStyles.styleMedium20(context),
+            "ابدأ الكتابة للبحث..",
+            style: AppStyles.styleMedium20(
+              context,
+            ).copyWith(color: Colors.white),
           ),
         );
   }
@@ -49,6 +54,78 @@ class CustomSearchDelegateEducational extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text("Ali");
+    if (query.isNotEmpty) {
+      BlocProvider.of<DatabaseCubit>(
+        context,
+      ).searchAllEducationalData(searchWord: query);
+    }
+    return query.isNotEmpty
+        ? BlocBuilder<DatabaseCubit, DatabaseState>(
+          builder: (context, state) {
+            if (state is SuccsesSearchDataEducational) {
+              if (state.itemStageModel.isEmpty) {
+                return Center(
+                  child: Text(
+                    "لا توجد نتائج",
+                    style: AppStyles.styleMedium20(
+                      context,
+                    ).copyWith(color: Colors.white),
+                  ),
+                );
+              }
+              return Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.kPadding18.w,
+                  vertical: SizeConfig.kPadding18.h,
+                ),
+                child: ListView.separated(
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    final item = state.itemStageModel[index];
+                    return ListTile(
+                      title: Text(
+                        item.name,
+                        style: AppStyles.styleBold20(
+                          context,
+                        ).copyWith(color: Colors.white),
+                      ),
+                      subtitle: Text(
+                        item.desc,
+                        style: AppStyles.styleMedium16(context),
+                      ),
+                      onTap: () {
+                        query = item.name;
+                        showResults(context);
+                      },
+                    );
+                  },
+                  separatorBuilder:
+                      (context, index) =>
+                          SizedBox(height: SizeConfig.kHeight16),
+                  itemCount: state.itemStageModel.length,
+                ),
+              );
+            } else if (state is LoadingSearchDataEducational) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return Center(
+                child: Text(
+                  "ابدأ الكتابة للبحث..",
+                  style: AppStyles.styleMedium20(
+                    context,
+                  ).copyWith(color: Colors.white),
+                ),
+              );
+            }
+          },
+        )
+        : Center(
+          child: Text(
+            "ابدأ الكتابة للبحث..",
+            style: AppStyles.styleMedium20(
+              context,
+            ).copyWith(color: Colors.white),
+          ),
+        );
   }
 }
