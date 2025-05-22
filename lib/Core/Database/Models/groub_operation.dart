354ee125f83feee4db3a5a287b3679506a97b208
alikasrawy <kasrawyali47@gmail.com>
2025-05-23 02:47:58 +0300

    create insert groub method to groub operation

--- lib/Core/Database/Models/groub_operation.dart
+++ lib/Core/Database/Models/groub_operation.dart
@@ -1,5 +1,12 @@
 import 'package:drosak_management/Core/Database/Local/sq_flite_database.dart';
+import 'package:drosak_management/Core/Helper/app_helper_groub.dart';
+import 'package:drosak_management/Featured/Layout/Models/groub_details_model.dart';
 
 class GroubOperation extends SqFliteDatabase {
-  
+  Future<bool> insertGroub(GroubDetailsModel model) async {
+    return await inseretData(
+      tableName: AppHelperGroub.groudTableName,
+      values: model.toJson(),
+    );
+  }
 }
